f0b505f956ba8f86709bb859525a025f03f18543
Samuel Velasco <svelasco514@gmail.com>
2024-03-21 18:40:44 -0400

    little go practice file
    I just realised I dont know go

---
+++ goDoThat/examples.go
@@ -0,0 +1,109 @@
+package main
+
+import "fmt"
+
+func main() {
+	weirdConditionals()	
+}
+
+func numericalPractice() {
+	const s string = "constant"
+	t := "Henlo"
+
+	fmt.Println(t)
+
+	fmt.Println("Hello world")
+
+
+
+	var a = "initial"
+	fmt.Println(a)
+	var b, c = 1, 2
+	fmt.Println(b)
+	fmt.Println(c)
+
+	asjodjasodijsadioidjsaoidj
+	aisdjoasjdoisajd
+	f := "Hello"
+	fmt.Println(f)
+
+
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	
+}
+
+func forPractice() {
+	
+		i :=  1 
+		for i <= 3 {
+			fmt.Println(i)
+			i += 1
+		}
+
+	for j := 0 ; j < 5 ; j++ {
+		fmt.Println(j)
+	}
+
+	for i := range 10 {
+		for j := range 5 {
+			fmt.Print(i , j)
+		}
+		fmt.Println()	
+	}
+}
+
+asdjadsijoaidsjoaisd
+func weirdConditionals() {
+	// normal conditional
+    	
+	var x int = 
+
+	if x < 10 {
+		fmt.Println(x, " is smaller than", 10)	
+	}
+
+
+	if y := 25; y < 0 {
+		fmt.Println("y is negative")
+	} else {
+		fmt.Println("y is positive")
+	}
+}
+
+
+
+func switchCasePractice {
+    
+    fmt.Println()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
